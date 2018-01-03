7aed935230196bd18405b2677146d881d1cf0e26
MR-TY <37832012+MR-TY@users.noreply.github.com>
2018-01-04 01:53:09 +0800

    Mybtis_MBG_Simple

---
+++ Mybatis_MBG/src/com/ty/test/TestNewMybatis.java
@@ -0,0 +1,50 @@
+package com.ty.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import com.ty.dao.EmployeeMapper;
+import com.ty.entity.Employee;
+
+public class TestNewMybatis {
+	public SqlSessionFactory getSqlSessionFactory() throws IOException {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		return new SqlSessionFactoryBuilder().build(inputStream);
+	}
+
+	@Test
+	public void testMBG() throws Exception, Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("mbg.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+	@Test
+	public void testCRUD() throws Exception {
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession session = sqlSessionFactory.openSession();
+		EmployeeMapper employeeMapper = session.getMapper(EmployeeMapper.class);
+		List<Employee> employee = employeeMapper.selectAll();
+		for (Employee employee2 : employee) {
+			System.out.println(employee2);
+		}
+	}
+}
